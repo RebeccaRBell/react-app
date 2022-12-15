447cdf9b43a76b53d4bd87850ac4e8a34957da7e
RebRomBell <112978622+RebRomBell@users.noreply.github.com>
2022-12-15 10:12:29 +0000

    updated for shecodes submission

--- src/CurrentDate.js
+++ src/CurrentDate.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function CurrentDate(props) {
    console.log(props.date);
-   let day = props.date.getDay();
     return(
     <div>
-        {day}
         </div>
     );
 }
